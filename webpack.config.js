8e5577d21b3d926a451e7e679a1b061102e18ff3
Francesco Bedussi <francesco@francescobedussi.it>
2017-02-11 13:59:28 +0100

    remove unnecessary babel transformations

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,32 @@ var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
 //var json = require('json-loader!./file.json');
 
 var options = {
-  entry: './lib/front/src/renderer.js',
-  output: {
-    path: './lib/front/',
-    filename: 'renderer-bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass!'
-      },{
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
-  devtool: 'source-map'
+	entry: './lib/front/src/renderer.js',
+	output: {
+		path: './lib/front/',
+		filename: 'renderer-bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					plugins: ["transform-es2015-modules-commonjs"],
+					presets: ['react']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style!css!sass!'
+			}, {
+				test: /\.json$/,
+				loader: 'json-loader'
+			}
+		]
+	},
+	devtool: 'source-map'
 };
 
 options.target = webpackTargetElectronRenderer(options);
